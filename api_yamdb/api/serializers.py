00cfd0a1b9514a6ab289fb16b8d834e2c121a6de
Ксения <ksucha98@gmail.com>
2023-02-02 15:38:09 +0300

    serializers added

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -23,3 +23,66 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         exclude = ('id',)
         model = Genre
+
+
+class TitlePostSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field="slug", queryset=Category.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field="slug", queryset=Genre.objects.all(), many=True
+    )
+
+    class Meta:
+        fields = (
+            'id', 'name', 'rating', 'description',
+            'year', 'category', 'genre'
+        )
+        model = Title
+
+
+class TitleGetSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    genre = GenreSerializer(many=True)
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        fields = (
+            'id', 'text', 'name', 'rating',
+            'year', 'category', 'genre', 'description'
+        )
+        model = Title
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    score = serializers.IntegerField(min_value=1, max_value=10)
+
+    def validate_review(self, validated_data):
+        count_review_title = Review.objects.filter(
+            author=validated_data.get('author'),
+            title=validated_data.get('title')
+        ).count()
+        if count_review_title == 1:
+            raise serializers.ValidationError(
+                'Нельзя оставлять больше одного отзыва'
+            )
+        return super().validate_review(validated_data)
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'name', 'score', 'pub_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
