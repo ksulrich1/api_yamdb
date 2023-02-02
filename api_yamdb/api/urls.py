ab3a154f5ad38e18ad584978a0274304a444c973
Хасан Циба <stooormy@yandex.ru>
2023-02-02 13:21:31 +0300

    Добавил комменты и  отзывы

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,25 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from .views import APIGetToken, APIUserSignup
+from .views import (APIGetToken, CommentViewSet,
+                    ReviewViewSet)
 
 router_v1 = DefaultRouter()
+router = SimpleRouter()
 router_v1.register()
+
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/auth/signup/', APIUserSignup.as_view(), name='api_signup'),
