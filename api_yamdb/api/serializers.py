de3e0a9ce77e6e740a1351712e68c5cdcf9ba25d
Аким Матвиенко <akimmatvienko@rambler.ru>
2023-02-09 23:30:04 +0300

    Ревью 1.1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -81,14 +81,13 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ("id", "text", "score", "pub_date", "author")
 
     def validate(self, data):
-        if not self.context.get("request").method == "POST":
-            return data
-        author = self.context.get("request").user
-        title_id = self.context.get("view").kwargs.get("title_id")
-        if Review.objects.filter(author=author, title=title_id).exists():
-            raise serializers.ValidationError(
-                "Вы уже оставляли отзыв на это произведение"
-            )
+        if self.context.get("request").method == "POST":
+            author = self.context.get("request").user
+            title_id = self.context.get("view").kwargs.get("title_id")
+            if Review.objects.filter(author=author, title=title_id).exists():
+                raise serializers.ValidationError(
+                    "Вы уже оставляли отзыв на это произведение"
+                )
         return data
 
 
